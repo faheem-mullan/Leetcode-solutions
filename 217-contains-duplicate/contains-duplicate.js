fd9f3035a8af06ecd1a4c953778a426c95d8e862
faheem-mullan <124805820+faheem-mullan@users.noreply.github.com>
2026-03-21 21:10:19 +0530

    Time: 19 ms (48.90%) | Memory: 74.4 MB (42.41%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.js
@@ -0,0 +1,13 @@
+
+var containsDuplicate = function(nums) {
+    const seen = new Set()
+    
+    for (const num of nums)
+    if(seen.has(num))
+    {
+        return true;
+    }else{
+        seen.add(num);
+    }
+    return false;
+}
